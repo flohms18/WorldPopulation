f4160013191c2c1c5f68de686c0e44fe5474b2f8
flotr18 <trouilletflorent@outlook.com>
2025-03-04 17:05:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('world_population.csv')
 
-GlobalWorldPopulation2022 = df['2022 Population']
-
+GlobalWorldPopulation2022 = 0
+for i in range(len(df)):
+    GlobalWorldPopulation2022 += df.loc[i, '2022 Population']
 print(GlobalWorldPopulation2022)
